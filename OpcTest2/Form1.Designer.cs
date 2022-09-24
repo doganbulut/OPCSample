4fb17f907a2299c5cf90ebf4540c1cb9a7ce9908
doganbulut <doganbulut@gmail.com>
2022-09-24 14:14:32 +0300

    Add project files.

---
+++ OpcTest2/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace OpcTest2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnWrite = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.lstLog = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lstUrlList = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trwServers = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(13, 13);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 1;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.Location = new System.Drawing.Point(104, 13);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(75, 23);
+            this.btnWrite.TabIndex = 2;
+            this.btnWrite.Text = "Write";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(186, 13);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 23);
+            this.btnRead.TabIndex = 3;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(268, 13);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Conn ??";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // lstLog
+            // 
+            this.lstLog.FormattingEnabled = true;
+            this.lstLog.Location = new System.Drawing.Point(12, 194);
+            this.lstLog.Name = "lstLog";
+            this.lstLog.Size = new System.Drawing.Size(582, 147);
+            this.lstLog.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "OPC Servers Connection URL";
+            // 
+            // lstUrlList
+            // 
+            this.lstUrlList.FormattingEnabled = true;
+            this.lstUrlList.Location = new System.Drawing.Point(12, 145);
+            this.lstUrlList.Name = "lstUrlList";
+            this.lstUrlList.Size = new System.Drawing.Size(582, 43);
+            this.lstUrlList.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "OPC Servers Properties";
+            // 
+            // trwServers
+            // 
+            this.trwServers.Location = new System.Drawing.Point(12, 57);
+            this.trwServers.Name = "trwServers";
+            this.trwServers.Size = new System.Drawing.Size(589, 63);
+            this.trwServers.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(603, 353);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lstUrlList);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trwServers);
+            this.Controls.Add(this.lstLog);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnWrite);
+            this.Controls.Add(this.btnConnect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnWrite;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox lstLog;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lstUrlList;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TreeView trwServers;
+    }
+}
+
---
+++ OpcTest2/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OpcTest2
+{
+    public partial class Form1 : Form
+    {
+        Opc.Da.Server server = null;
+        OpcCom.Factory fact = new OpcCom.Factory();
+        Opc.Da.Item[] items;
+        Opc.Da.Subscription group;
+        Opc.IRequest req;
+        Opc.Da.WriteCompleteEventHandler WriteEventHandler;
+        Opc.Da.ReadCompleteEventHandler ReadEventHandler;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void GetOpcServers()
+        {
+            try
+            {
+                OpcCom.ServerEnumerator se = new OpcCom.ServerEnumerator();
+
+                Opc.Server[] servers = se.GetAvailableServers(Opc.Specification.COM_DA_20);
+
+                ListServers(servers);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private void ListServers(Opc.Server[] OpsServerList)
+        {
+            trwServers.Nodes.Clear();
+            lstUrlList.Items.Clear();
+            foreach (Opc.Server serv in OpsServerList)
+            {
+                TreeNode trn = new TreeNode(serv.Name);
+                trn.Nodes.Add(serv.Url.HostName + " : " + serv.Url.Path + " : " + serv.Url.Port);
+                trn.Nodes.Add(serv.Url.ToString());
+                trn.Nodes.Add(serv.IsConnected.ToString());
+                trwServers.Nodes.Add(trn);
+                lstUrlList.Items.Add(serv.Url.ToString());
+            }
+        }
+
+
+        private bool ConnectOPCServer(string OpcUrl)
+        {
+            // Create a server object and connect to the TwinCATOpcServer
+            Opc.URL url = new Opc.URL(OpcUrl);
+            
+            server = new Opc.Da.Server(fact, null);
+
+            try
+            {
+                server.Connect(url, new Opc.ConnectData(new System.Net.NetworkCredential()));
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show(Ex.Message);
+                return false;
+            }
+
+            // Okuma Grubu 
+            Opc.Da.SubscriptionState groupState = new Opc.Da.SubscriptionState();
+            groupState.Name = "Group1";
+            groupState.Active = true;
+
+
+            group = (Opc.Da.Subscription)server.CreateSubscription(groupState);
+            group.DataChanged += new Opc.Da.DataChangedEventHandler(group_DataChanged);
+
+
+            // Gruba 3 adet OPC değişkeni ekle
+            items = new Opc.Da.Item[3];
+            items[0] = new Opc.Da.Item();
+            items[0].ItemName = "Bucket Brigade.Int1";
+            items[1] = new Opc.Da.Item();
+            items[1].ItemName = "Bucket Brigade.Int2";
+            items[2] = new Opc.Da.Item();
+            items[2].ItemName = "Bucket Brigade.Int3";
+            items = group.AddItems(items);
+
+            //Write Callback fonksiyonumuzu tutacak handler 
+            WriteEventHandler = new Opc.Da.WriteCompleteEventHandler(WriteCompleteCallback);
+             //Read Callback fonksiyonumuzu tutacak handler 
+            ReadEventHandler = new Opc.Da.ReadCompleteEventHandler(ReadCompleteCallback);
+
+            return true;
+        }
+
+        void group_DataChanged(object subscriptionHandle, object requestHandle, Opc.Da.ItemValueResult[] values)
+        {
+            foreach (Opc.Da.ItemValueResult chitem in values)
+            {
+                WriteLogList(chitem.ItemName + " : " +
+                                chitem.ItemPath + " : " +
+                                chitem.Key + " : " +
+                                chitem.Value);
+            }
+        }
+
+
+        void WriteLogList(string item)
+        {
+            lstLog.BeginInvoke((MethodInvoker)delegate
+            {
+                lstLog.Items.Insert(0, item);
+            });
+        }
+
+        void WriteCompleteCallback(object clientHandle, Opc.IdentifiedResult[] results)
+        {
+
+            WriteLogList("Write completed");
+            foreach (Opc.IdentifiedResult writeResult in results)
+            {
+                WriteLogList(writeResult.ItemName + " : " + writeResult.ResultID);
+            }
+
+        }
+
+        void ReadCompleteCallback(object clientHandle, Opc.Da.ItemValueResult[] results)
+        {
+            WriteLogList("Read completed");
+            foreach (Opc.Da.ItemValueResult readResult in results)
+            {
+                WriteLogList(readResult.ItemName + " : " + readResult.Value);
+            }
+            Console.WriteLine();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetOpcServers();   
+        }
+
+        
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (server.IsConnected)
+            {
+                MessageBox.Show("connected");
+            }
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if ((server != null) && (server.IsConnected))
+                server.Disconnect();
+
+            if (lstUrlList.SelectedItems.Count != 0)
+                ConnectOPCServer(lstUrlList.SelectedItem.ToString());
+        }
+
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            Opc.Da.ItemValue[] iv = new Opc.Da.ItemValue[group.Items.Count()];
+            Random rnd = new Random();
+
+
+            for (int i = 0; i < group.Items.Count(); i++)
+            {
+                Opc.Da.ItemValue iiv = new Opc.Da.ItemValue(group.Items[i].ItemName);
+                iiv.ItemPath = group.Items[i].ItemPath;
+                iiv.ServerHandle = group.Items[i].ServerHandle;
+                iiv.Value = rnd.Next(1,1000);
+                iv[i] = iiv;
+            }
+
+            //group.Write(iv);
+            //Asenkron
+            group.Write(iv, 1234, WriteCompleteCallback, out req);
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            //group.Read(group.Items);
+            //and now read the items again
+            group.Read(group.Items, 123, ReadCompleteCallback, out req);
+        }
+
+        
+
+       
+    }
+}
